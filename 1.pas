85ce49582f20e21e0187dee44f8409b2d76e1033
я саша <144239395+aleksashaaleksasha@users.noreply.github.com>
2023-11-07 13:08:56 +0300

    Add files via upload

---
+++ 1.pas
@@ -0,0 +1,24 @@
+﻿var k,s,length,i,sum : integer; ff : boolean;
+A: array of integer;
+begin
+  ff := False;
+  write('Введите длину массива А: ');
+  readln(length);
+  SetLength(A, length);
+  writeln('Введите элементы массива А:');
+  for i := 0 to length-1 do begin
+    readln(A[i]);
+  end;
+  write('Введите число s: ');
+  readln(s);
+  while sum <= s do begin
+    sum := sum + A[k];
+    k := k + 1;
+    if (k = length-1) and (sum <= s) then begin
+      ff := True;
+      break;
+    end;
+  end;
+  if ff = False then writeln('Количество элементов, нужное чтобы их сумма была больше заданного числа s = ', k, '.')
+  else writeln('Нет стольких элементов, чтобы их сумма была больше чем s.');
+end.
---
+++ 2.pas
@@ -0,0 +1,17 @@
+﻿var s,c: string; i,j,cnt,k,cntmax : integer;
+A : array of string;
+begin
+  writeln('Введите слова через пробел:');
+  readln(s);
+  SetLength(A,length(s));
+  for i := 1 to length(s) do if not (s[i] = ' ') then A[k] := A[k] + s[i] else  k := k + 1;
+  //writeln(A);
+  for j := 1 to length(A)-1 do begin
+    //Ыwriteln(A.CountOf(A[j]));
+    if (cntmax < A.CountOf(A[j])) and (A[j] <> '') then begin
+    cntmax := A.CountOf(A[j]);
+    c := A[j];
+    end;
+  end;
+  writeln('Наибольшее встречающееся слово в строке – это "', c, '". Кол-во повторений равно ', cntmax,'.');
+end.
